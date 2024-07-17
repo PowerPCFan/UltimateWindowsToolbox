5b7b2b117980f129278b3b1be057f13ee6fe7616
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-07-17 07:18:50 -0400

    Changed temporary folder from %windir%\temp to %temp%

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile C:\windows\temp\ultimatewindowstoolbox.cmd
+Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile %temp%\ultimatewindowstoolbox.cmd
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-powershell.exe "C:\windows\temp\ultimatewindowstoolbox.cmd"
+powershell.exe "%temp%\ultimatewindowstoolbox.cmd"
