350d5c5a3018c67a6e52fbc17105f1d4f8f6f948
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-07-17 07:43:51 -0400

    please just work this time!! (fixing paths)

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,3 +1,5 @@
-Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile C:\Users\$Env:UserName\AppData\Local\Temp\ultimatewindowstoolbox.cmd
+$username = $env:UserName
+
+Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile C:\Users\$username\AppData\Local\Temp\ultimatewindowstoolbox.cmd
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-powershell.exe "C:\Users\$Env:UserName\AppData\Local\Temp\ultimatewindowstoolbox.cmd"
+powershell.exe "C:\Users\$username\AppData\Local\Temp\ultimatewindowstoolbox.cmd"
