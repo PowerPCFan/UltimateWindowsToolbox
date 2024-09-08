cb87ea3c24f9edf080c7ac49ae0614732310a270
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-09-08 13:09:42 -0400

    Update wintoolbox.ps1

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,5 +1,6 @@
 $temp = $env:Temp
 
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "$temp\ultimatewindowstoolbox.cmd"
+Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "$temp\welcome.txt"
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 powershell.exe "$temp\ultimatewindowstoolbox.cmd"
