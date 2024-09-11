260312d222de22d2a8b360c0a2635368c4b6ba06
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-09-11 18:10:53 -0400

    ANOTHER file path bug fix lol

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,5 +1,8 @@
 $temp = $env:Temp
 
+rmdir "$temp\UltimateWindowsToolbox"
+mkdir "$temp\UltimateWindowsToolbox"
+
 Remove-Item "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
 Remove-Item "$temp\UltimateWindowsToolbox\welcome.txt"
 
