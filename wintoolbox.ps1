ab3c69b6f2fabdeeb5c033b24cda144fa9b29e17
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-09-11 18:09:09 -0400

    Critical filepath bug fix

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,10 +1,10 @@
 $temp = $env:Temp
 
-Remove-Item "$temp\ultimatewindowstoolbox.cmd"
-Remove-Item "$temp\welcome.txt"
+Remove-Item "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
+Remove-Item "$temp\UltimateWindowsToolbox\welcome.txt"
 
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "$temp\UltimateWindowsToolbox\welcome.txt"
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-powershell.exe "$temp\ultimatewindowstoolbox.cmd"
+powershell.exe "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
