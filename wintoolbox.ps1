eb6927a5d8a70bcfc7459f780009a1ab14f0cb6d
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-09-11 20:26:58 -0400

    i think i finally fixed it lmao

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,13 +1,11 @@
 $temp = $env:Temp
 
-rmdir "%temp%\UltimateWindowsToolbox"
-mkdir "%temp%\UltimateWindowsToolbox"
-
-Remove-Item "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
-Remove-Item "$temp\UltimateWindowsToolbox\welcome.txt"
-
-Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
-Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "$temp\UltimateWindowsToolbox\welcome.txt"
+cd $temp
+rmdir "UltimateWindowsToolbox"
+mkdir "UltimateWindowsToolbox"
+cd "UltimateWindowsToolbox"
+Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "ultimatewindowstoolbox.cmd"
+Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "welcome.txt"
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-powershell.exe "$temp\UltimateWindowsToolbox\ultimatewindowstoolbox.cmd"
+powershell.exe ".\ultimatewindowstoolbox.cmd"
