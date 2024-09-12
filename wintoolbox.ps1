955b7a09e508ae1d5afa525446da59609f5e7fcd
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-09-11 20:34:09 -0400

    Added -Force to execution policy

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -10,5 +10,5 @@ cd "UltimateWindowsToolbox"
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "ultimatewindowstoolbox.cmd"
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "welcome.txt"
 
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 powershell.exe ".\ultimatewindowstoolbox.cmd"
