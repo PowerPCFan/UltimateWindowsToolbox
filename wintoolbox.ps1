f2cf8096f41e38cd75f454fa55b15fe1483baa82
PowerPCFan <charlies.retro.computing@gmail.com>
2024-09-28 07:50:30 -0400

    Converts LF to CR LF with Powershell

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -10,5 +10,8 @@ Set-Location "UltimateWindowsToolbox"
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "ultimatewindowstoolbox.cmd"
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "welcome.txt"
 
+# Converts LF (Unix) line endings to CR LF (Windows)
+(Get-Content "UltimateWindowsToolbox.cmd" -Raw) -replace "`n", "`r`n" | Set-Content "UltimateWindowsToolbox.cmd"
+
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 powershell.exe ".\ultimatewindowstoolbox.cmd"
