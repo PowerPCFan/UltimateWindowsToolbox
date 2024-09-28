08179bec1b58077d28c13da9f5f7e14c8ffb32db
PowerPCFan <charlies.retro.computing@gmail.com>
2024-09-28 07:54:07 -0400

    Added comments

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,17 +1,24 @@
+# Set variables
 $temp = $env:Temp
 
+# Changes directory to %temp%
 Set-Location $temp
 
+# Deletes old Windows Toolbox files if you've ran it before
 Remove-Item -Recurse -Force -Confirm:$false "UltimateWindowsToolbox"
+# Creates new directory for Windows Toolbox
 New-Item -Name "UltimateWindowsToolbox" -Type Directory
 
+# Sets location to the windows toolbox folder
 Set-Location "UltimateWindowsToolbox"
 
+# Downloading scripts and prerequisites
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "ultimatewindowstoolbox.cmd"
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "welcome.txt"
 
 # Converts LF (Unix) line endings to CR LF (Windows)
 (Get-Content "UltimateWindowsToolbox.cmd" -Raw) -replace "`n", "`r`n" | Set-Content "UltimateWindowsToolbox.cmd"
 
+# Change execution policy and run script
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 powershell.exe ".\ultimatewindowstoolbox.cmd"
