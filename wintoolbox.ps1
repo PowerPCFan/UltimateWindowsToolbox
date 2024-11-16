12a3bde6e1adfd5a310d14220e4db16dbfc325b4
PowerPCFan <121565961+PowerPCFan@users.noreply.github.com>
2024-11-16 16:56:44 -0500

    changed comments in wintoolbox.ps1 because i felt like it :)

--- wintoolbox.ps1
+++ wintoolbox.ps1
@@ -1,24 +1,27 @@
-# Set variables
+# Set variables:
 $temp = $env:Temp
 
-# Changes directory to %temp%
+# Changes directory to the Windows temp folder.
 Set-Location $temp
 
-# Deletes old Windows Toolbox files if you've ran it before
+# Deletes old Windows Toolbox files to avoid conflicts if you've run the script before.
 Remove-Item -Recurse -Force -Confirm:$false "UltimateWindowsToolbox"
-# Creates new directory for Windows Toolbox
+# Creates new directory for UltimateWindowsToolbox's files and scripts
 New-Item -Name "UltimateWindowsToolbox" -Type Directory
 
 # Sets location to the windows toolbox folder
 Set-Location "UltimateWindowsToolbox"
 
-# Downloading scripts and prerequisites
+# Downloads main script
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/ultimatewindowstoolbox.cmd -OutFile "ultimatewindowstoolbox.cmd"
+# Downloads welcome.txt - welcome message for the main script
 Invoke-WebRequest https://raw.githubusercontent.com/PowerPCFan/UltimateWindowsToolbox/main/welcome.txt -OutFile "welcome.txt"
 
-# Converts LF (Unix) line endings to CR LF (Windows)
+# Converts LF/Line Feed (Unix) line endings to CR+LF/Carriage Return + Line Feed (Windows) to ensure the script runs correctly
 (Get-Content "UltimateWindowsToolbox.cmd" -Raw) -replace "`n", "`r`n" | Set-Content "UltimateWindowsToolbox.cmd"
 
-# Change execution policy and run script
+# Changes PowerShell's execution policy to Unrestricted
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+
+# Runs the script
 powershell.exe ".\ultimatewindowstoolbox.cmd"
